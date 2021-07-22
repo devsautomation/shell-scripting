647bb0443353e476b8724804071f6f37d6b2ea79
DevsAutomation <deviprasaddas.blr@gmail.com>
2021-07-22 23:58:09 +0530

    funct

--- 03-function.sh
+++ 03-function.sh
@@ -7,10 +7,11 @@ test()
   echo This is my test funct
   echo value of a = ${a}
   b=200
+  echo first argument in the function $1
 }
 
 #call func in Main Prog
 a=100
-test
-
+test ABC
 echo value of b = ${b}
+echo first argument in the min prog $1
