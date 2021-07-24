526f2cb8cfcb14378187e94ec3277784b4e5237c
DevsAutomation <deviprasaddas.blr@gmail.com>
2021-07-25 00:10:19 +0530

    frontend

---
+++ roboshop/common.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+USER_ID=$(id-u)
+if [ $USER_ID -ne 0 ];then
+echo you shouls be root user to run the script
+exit 2
+fi
+
+
+LOG=/tmp/roboshop.log
+rm -f $LOG
+
+status_check()
+{
+if [ $1 -eq 0 ];then
+echo done
+else
+echo  fail
+exit 1
+fi
+}
---
+++ roboshop/frontend.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+source common.sh
+
+echo "Enabling nginx"
+yum install nginx -y &>>$LOG
+status_check $?
+
+echo "Download Frontend"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG
+status_check $?
+
+
+echo "remove old htdocs"
+cd /usr/share/nginx/html &>>$LOG && rm -rf * &>>$LOG
+status_check $?
+
+echo "Extract front end archieve AND MOVE TO DESIRED LOCATION"
+unzip /tmp/frontend.zip &>>$LOG &&mv frontend-main/* . &>>$LOG && mv static/* . &>>$LOG && rm -rf frontend-master static &>>$LOG
+status_check $?
+
+echo "update roboshop config"
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG
+status_check $?
+
+
+echo "Enabling nginx"
+systemctl enable nginx &>>$LOG
+status_check $?
+
+
+echo "Starting nginx"
+systemctl start nginx &>>$LOG
+status_check $?
