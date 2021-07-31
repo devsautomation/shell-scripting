cae6e1c6a0764bf462c986cd725f11c5b38f1d5d
DevsAutomation <deviprasaddas.blr@gmail.com>
2021-07-31 20:03:22 +0530

    TestCase

--- loop.sh
+++ loop.sh
@@ -13,3 +13,16 @@ done
 for fruit in apple banana orange kiwi ;do
   echo $fruit
    done
+
+#Case in Shell
+FRUIT="kiwi"
+case "$FRUIT" in
+   "apple") echo "Apple pie is quite tasty."
+   ;;
+   "banana") echo "I like banana nut bread."
+   ;;
+   "kiwi") echo "New Zealand is famous for kiwi."
+   ;;
+  *) echo "No such categ"
+   ;;
+esac
