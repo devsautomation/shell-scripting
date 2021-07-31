ca7dfa36c5b10fd2d419ab37d913c5511f0f8d43
DevsAutomation <deviprasaddas.blr@gmail.com>
2021-07-31 23:57:33 +0530

    tera

---
+++ terraform/variables/vars.tf
@@ -0,0 +1,18 @@
+variable "sample" {
+  default = "Hello World"
+}
+
+output "sample" {
+  value = var.sample
+}
+
+output "sample1" {
+  value = "{$var.sample} -From Terraform"
+}
+
+variable "sample2" {}
+
+output "sample2" {
+  value = var.sample2
+}
+
